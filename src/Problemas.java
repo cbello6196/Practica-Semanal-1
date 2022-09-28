d124b59c9f0551290716e6a279b89cea944ce968
abraham1798 <abrahamc.a@hotmail.com>
2022-09-27 22:25:13 -0600

    Se actualiza linea de codigo

--- src/Problemas.java
+++ src/Problemas.java
@@ -2,71 +2,82 @@
 import java.util.Arrays;
 import java.util.List;
 
-
 public class Problemas {
-   public String Quitarvocal(String str)
-    {
-         Character vocales[] = {'a', 'e', 'i', 'o', 'u','A','E','I','O','U'};
-          
-         List<Character> al = Arrays.asList(vocales);
-          
-         StringBuffer sb = new StringBuffer(str);
-          
-         for (int i = 0; i < sb.length(); i++) {
-             
-             if(al.contains(sb.charAt(i))){
-                sb.replace(i, i+1, "") ;
+
+    public String Quitarvocal(String str) {
+        Character vocales[] = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+
+        List<Character> al = Arrays.asList(vocales);
+
+        StringBuffer sb = new StringBuffer(str);
+
+        for (int i = 0; i < sb.length(); i++) {
+
+            if (al.contains(sb.charAt(i))) {
+                sb.replace(i, i + 1, "");
                 i--;
-             }
-        }        
+            }
+        }
         return sb.toString();
     }
-    public int ProblemaB(int n) { 
-        int suma = 0; 
-        for (int i = 1; i <= n; i++) { 
-        suma = suma + i;
-        System.out.printf("total: %d\n",suma);    
-        } 
-        return suma; 
+
+    public int ProblemaB(int n) {
+        int suma = 0;
+        for (int i = 1; i <= n; i++) {
+            suma = suma + i;
+            System.out.printf("total: %d\n", suma);
+        }
+        return suma;
     }
-    
-         public double calcExponencial(int n, int x){
-      double result1 =0;
-      double result2=0;
-      for (int i=0; i<n; i++){
-          result1 = result1-(1) + (Math.pow(x,i) / factorial(i));
-      }
-     
-      
-       for (int i=0; i<n; i++){
-          result2 = result2 + (Math.pow(x,i) / factorial(i));
-      }
-        System.out.printf("Resultado del calculo exponencial sin recursividad:" + result2+"\n"+"Resultado del calculo exponencial con recursivada:"+ result1);
+
+    public double calcExponencial(int n, int x) {
+        double result1 = 0;
+        double result2 = 0;
+        for (int i = 0; i < n; i++) {
+            result1 = result1 - (1) + (Math.pow(x, i) / factorial(i));
+        }
+
+        for (int i = 0; i < n; i++) {
+            result2 = result2 + (Math.pow(x, i) / factorial(i));
+        }
+        System.out.printf("Resultado del calculo exponencial sin recursividad:" + result2 + "\n" + "Resultado del calculo exponencial con recursivada:" + result1);
         return result1;
     }
-      public double factorial (int num){
-           double result1 =0;
-           double result2=0;
-          for (int i=1; i<=num; i++){
-              result1 = result1-(1) *i;
-          }
-           for (int i=1; i<=num; i++){
-              result2 = result2 *i;
-          }
-           System.out.printf("Resultado del calculo factorial sin recursividad:" + result2+"\n"+"Resultado del calculo factorial con recursivada:"+ result1);
-          return result1; 
-      }
-       
+
+    public double factorial(int num) {
+        double result1 = 0;
+        double result2 = 0;
+        for (int i = 1; i <= num; i++) {
+            result1 = result1 - (1) * i;
+        }
+        for (int i = 1; i <= num; i++) {
+            result2 = result2 * i;
+        }
+        System.out.printf("Resultado del calculo factorial sin recursividad:" + result2 + "\n" + "Resultado del calculo factorial con recursivada:" + result1);
+        return result1;
+    }
 
     public int ProblemaE(int num) {
-        if (num<2) {
+        if (num < 2) {
             return num;
         } else {
-            int numeros = num % 2 + ProblemaE(num / 2)*10;
+            int numeros = num % 2 + ProblemaE(num / 2) * 10;
             return numeros;
         }
 
     }
-  
-}
 
+    public boolean ProblemaC(int numero) {
+        if (numero > 9) {
+            if (numero % 10 == 0 || numero % 10 == 1) {
+                return ProblemaC(numero / 10);
+            } else {
+                return false;
+            }
+        } else if (numero == 0 || numero == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
